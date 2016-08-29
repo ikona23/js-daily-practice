e32a02ba969ea6d6a01303351231613f9f249544
Peter Kona <ikona23@gmail.com>
2016-08-29 17:19:09 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,122 @@
 window.onload = function()
 {
 
+
+
+
+// simple objects
+
+function makeObject (key, value) {
+  var actual = {}
+  actual[key] = value;
+  return actual;
+}
+
+function getValue (obj, key) {
+}
+
+function ageOneYear (obj) {
+  obj.age +=1;
+  return obj;
+}
+
+function deleteProp (obj, key) {
+  delete obj[key];
+  return obj;
+}
+
+// simple arrays
+
+function makeArrayOfItem (item, length) {
+  var arr = [length];
+  for (var i = 0; i < length; i++) {
+    arr[i] = item;
+  }
+  return arr;
+}
+
+function makeArrayOfItems () {
+  return Array.from(arguments);
+}
+
+function getGreeting (name) {
+  return "Hello " + name;
+}
+
+function returnErrorIfFalsy(args) {
+  if(!args) {
+    return obj = {
+      name: 'Error',
+      message: 'Oh no an error!'
+    }
+  }
+  return true;
+}
+
+// TESTS
+
+var expectedObj = { a: 1 }        // this is what we expect our function to make
+var actual = makeObject('a', 1)   // this is what our function actually makes
+
+console.log( "makeObject makes objects: ", actual.a === expectedObj.a )     // our 'test' : when it's passing, we will console.log true
+console.log( "getValue gets values from objects: ", getValue(expectedObj, 'a') === 1 )
+
+
+var mickey = { name: 'Mickey Mouse', age: 64, email: 'mickey@disney.com' }
+console.log( "ageOneYear adds 1 year to the age property of an object: ", ageOneYear(mickey).age === 65 )
+
+
+deleteProp(mickey, 'email')
+console.log( typeof mickey.email === 'undefined' )
+
+
+var expectedArray = [ 'a', 'a', 'a' ]
+var actualArray = makeArrayOfItem('a', 3)
+
+for (var i = 0; i < expectedArray.length; i++) {
+  console.log( expectedArray[i] === actualArray[i] )
+}
+
+
+var arrayOfDifferentItems = [ 'a', 2, false ]
+var actualArrayOfDifferent = makeArrayOfItems('a', 2, false)
+
+for (var i = 0; i < expectedArray.length; i++) {
+  console.log( arrayOfDifferentItems[i] === actualArrayOfDifferent[i] )
+}
+
+
+var names = [ 'Alice', 'Bob', 'Celia', 'Dan' ]
+var expectedGreetings = [ 'Hello Alice', 'Hello Bob', 'Hello Celia', 'Hello Dan' ]
+var actualGreetings = names.map(getGreeting)
+
+for (var i = 0; i < names.length; i++) {
+  console.log( actualGreetings[i] === expectedGreetings[i] )
+}
+
+
+var people = [ { name: 'Alice' }, { name: 'Bob' }, { name: 'Celia' }, { name: 'Dan' } ]
+var expectedPeopleWithGreetings = [
+  { name: 'Alice', greeting: 'Hello Alice' },
+  { name: 'Bob', greeting: 'Hello Bob' },
+  { name: 'Celia', greeting: 'Hello Celia' },
+  { name: 'Dan', greeting: 'Hello Dan' }
+]
+
+var actualPeopleWithGreetings = people.map(function (person) {
+  person.greeting = getGreeting(person.name)
+  return person
+})
+
+for (var i = 0; i < expectedPeopleWithGreetings.length; i++) {
+  console.log( expectedPeopleWithGreetings[i].greeting === actualPeopleWithGreetings[i].greeting )
+}
+var err = returnErrorIfFalsy(false)
+console.log( err.name === 'Error' && err.message === 'Oh no an error!' )
+
+var notErr = returnErrorIfFalsy({})
+console.log( notErr === true )
+
   //
   // function inArray (arrayToCheck, value){
   //   for (i = 0; i < arrayToCheck.length;i++){
