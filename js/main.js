1ba6ef96e5141d2da60abef7c6eb1a3f2e0a1693
Peter Kona <ikona23@gmail.com>
2016-08-30 16:13:37 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,190 @@
-window.onload = function()
-{
+var assert = require('./utils/assert')
+var data = require('./data/data')
+var expectedArrayOfArrays = require('./data/array-of-arrays')
+var expectedFormattedDates = require('./data/formatted-dates')
+
+var meaningOfLife = '42'
+
+function each (func, arr) {
+  for (var i = 0; i < arr.length; i++) {
+    func(arr[i])
+  }
+}
+
+/*
+ * getType
+ */
+
+function getType (thing) {
+  return typeof thing
+}
+
+assert(getType(meaningOfLife), 'string', 'meaningOfLife is a string data type')
+assert(getType(data), 'object', 'data is an object')
+
+/*
+ * isNumber
+ */
+
+function isNumber (thing) {
+  if (typeof thing === "number") {
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+assert(isNumber(meaningOfLife), false, 'meaningOfLife is not a number datatype')
+assert(isNumber(true), false, 'meaningOfLife is not a number datatype')
+assert(isNumber([2, 3]), false, 'meaningOfLife is not a number datatype')
+assert(isNumber(2), true, 'meaningOfLife is not a number datatype')
+
+/*
+ * isStringNumber
+ */
+
+function isStringNumber (str) {
+  if (!isNaN(str))
+  {
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+assert(isStringNumber(meaningOfLife), true, 'meaningOfLife to string number')
+assert(isStringNumber('jsksk'), false, 'isStringNumber does not give a false positive')
+
+/*
+ * toNumber
+ */
+
+function toNumber (str) {
+  return parseInt(str)
+}
+
+assert(toNumber(meaningOfLife), 42, 'toNumber can convert strings to number if possible')
+
+/*
+ * add
+ */
+
+function add (a, b) {
+  return a+b
+}
+
+assert(add(2, 3), 5, 'add successfully adds two numbers')
+
+/*
+ * addStrings
+ */
+
+function addStrings (a, b) {
+  var num = (parseInt(a) + parseInt(b))
+  return num.toString()
+  // if(isNan(a)){
+  //   if(isNan(b)){
+  //     return (parseInt(a) + parseInt(b))
+  //   }
+  //   else {
+  //     return a + parseInt(b)
+  //   }
+  //   return parseInt(a) + b
+  // }
+  // else {
+  //   return a + b
+  // }
+
+}
+
+assert(addStrings(meaningOfLife, '10'), '52', 'addStrings can add number strings and convert them back to a string')
 
 
+/*
+ * addStringsOrNumbers
+ */
+
+function addStringsOrNumbers (a, b) {
+if ((typeof a === 'string')||(typeof b === 'string')){
+  var num = (parseInt(a) + parseInt(b))
+  return num.toString()
+}else{
+
+  return (parseInt(a) + parseInt(b))
+}
+}
+
+assert(addStringsOrNumbers(2, 3), 5, 'addStringsOrNumbers can add numbers')
+assert(addStringsOrNumbers('1', '2'), '3', 'addStringsOrNumbers can add strings')
+assert(addStringsOrNumbers('10', 10), '20', 'addStringsOrNumbers can add strings and numbers (returning a string)')
+
+/*
+ * isEmail
+ */
+
+function isEmail (str) {
+return (str.includes('.')&&str.includes('@'))
+}
+
+assert(isEmail('thedonald@makeamericagreatagain.com'), true, 'isEmail detects an email')
+assert(isEmail('3333@'), false, 'isEmail does not give a false positive')
+assert(isEmail('johnny.b.good'), false, 'isEmail does not give a false positive')
+
+/*
+ * countIf
+ */
+
+function countIf (testFunc, arr) {
+  var count = 0
+  for(var i = 0; i<=arr.length; i++){
+    if(testFunc(arr[i])){
+      count++
+    }
+  }
+  return count
+}
+
+var isString = function (s) {
+  return typeof s === 'string'
+}
+var mixedArray = [1, '21', null, Date.now(), 5, meaningOfLife, 42]
+var expectedNumberCount = 4 // do you know which 4 are numbers?
+var expectedStringCount = 2
+var numberCount = countIf(isNumber, mixedArray)
+var stringCount = countIf(isString, mixedArray)
+
+assert(numberCount, expectedNumberCount, 'countIf can count the numbers in an array')
+assert(stringCount, expectedStringCount, 'countIf can count the strings in an array')
+
+/*
+ * filter
+ */
+
+function filter (func, arr) {
+  var newArray = arr.filter(isEmail)
+  return newArray
+
+}
+// console.log(data)
+var emails = filter(isEmail, data)
+assert(emails.length, 44, 'filter and isEmail returns the correct number of emails')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-----------------------------------
 
 
 // simple objects
